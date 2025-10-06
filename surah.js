0caf0bf748793d4711920f6f693753f47ac14580
singgihperkasa <76107369+singgihperkasa@users.noreply.github.com>
2025-10-06 22:31:17 +0700

    Update surah.js

--- surah.js
+++ surah.js
@@ -1,1 +1,11 @@
+fetch('https://equran.id/api/surat')
+  .then(response => response.json())
+  .then(data => {
+    const list = document.getElementById('surah-list');
+    data.forEach(surah => {
+      const item = document.createElement('li');
+      item.innerHTML = `<a href="surah-${surah.nomor}.html">${surah.nomor}. ${surah.nama_latin} (${surah.jumlah_ayat} ayat)</a>`;
+      list.appendChild(item);
+    });
+  });
 
